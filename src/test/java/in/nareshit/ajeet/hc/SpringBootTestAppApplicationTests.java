5e450d5d026df9765a01e2faf1f8345d28b7f762
AJEET <ajeetspringboot@gmail.com>
2022-02-01 09:57:38 +0530

    wh-8520-First Time File Update

---
+++ src/test/java/in/nareshit/ajeet/hc/SpringBootTestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.nareshit.ajeet.hc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTestAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
